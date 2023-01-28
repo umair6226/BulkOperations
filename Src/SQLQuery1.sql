8e73e00626d915ee4f69ec36df19040ec7d9e63a
Umair Gul <115822650+umair6226@users.noreply.github.com>
2023-01-28 17:43:07 +0500

    BulkOperations

---
+++ Src/SQLQuery1.sql
@@ -0,0 +1,12 @@
+
+create database db_BulkOperation
+
+create table ProductsSold(
+ProductName nvarchar(max),
+BrandName nvarchar(max),
+Warrenty nvarchar(max),
+Price nvarchar(max)
+)
+
+
+select*from ProductsSold
